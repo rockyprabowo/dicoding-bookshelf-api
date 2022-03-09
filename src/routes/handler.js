d5383e36e3afd1524c3daeddee6783d2f958d4c3
Rocky Prabowo <rocky@lazycats.id>
2022-03-09 23:17:36 +0700

    feat: Implement Create and Update handler

--- src/routes/handler.js
+++ src/routes/handler.js
@@ -1,7 +1,74 @@
-const addBookHandler = () => {}
+const Books = require('../data/book')
+
+const addBookHandler = (request, h) => {
+  const newBook = Books.sanitizePayload(request.payload)
+
+  const { success, id, error } = Books.add(newBook)
+
+  if (success) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    })
+    response.code(201)
+    return response
+  }
+
+  if (error.length > 0) {
+    const response = h.response({
+      status: 'fail',
+      message: `${error[0].failureMessage}`
+    })
+    response.code(400)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan'
+  })
+  response.code(500)
+  return response
+}
+
 const getAllBookHandler = () => {}
 const getBookHandler = () => {}
-const updateBookHandler = () => {}
+
+const updateBookHandler = (request, h) => {
+  const { id } = request.params
+  const bookData = Books.sanitizePayload(request.payload)
+
+  const { success, error } = Books.update(id, bookData)
+
+  if (success) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui'
+    })
+    response.code(201)
+    return response
+  }
+
+  if (error.length > 0) {
+    const response = h.response({
+      status: 'fail',
+      message: `${error[0].failureMessage}`
+    })
+    response.code(400)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
 const removeBookHandler = () => {}
 
 module.exports = { addBookHandler, getAllBookHandler, getBookHandler, updateBookHandler, removeBookHandler }
