133b7f0d5ce36b3c59b3edb7139cfdb89f630ce1
Rocky Prabowo <rocky@lazycats.id>
2022-03-09 16:12:54 +0700

    Initial project code

---
+++ src/routes/handler.js
@@ -0,0 +1,7 @@
+const addBookHandler = () => {}
+const getAllBookHandler = () => {}
+const getBookHandler = () => {}
+const updateBookHandler = () => {}
+const deleteBookHandler = () => {}
+
+module.exports = { addBookHandler, getAllBookHandler, getBookHandler, updateBookHandler, deleteBookHandler }
---
+++ src/routes/route.js
@@ -0,0 +1,31 @@
+const { addBookHandler, getAllBookHandler, getBookHandler, updateBookHandler, deleteBookHandler } = require('./handler')
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler
+  },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookHandler
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBookHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookHandler
+  }
+]
+
+module.exports = routes
